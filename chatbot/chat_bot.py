66406b815eb2c3641291ce2b93948c5b1d88bda9
KevinAlexandro <kevin-951206@hotmail.com>
2025-03-29 13:25:11 -0500

    this is a v0.1

--- chatbot/chat_bot.py
+++ chatbot/chat_bot.py
@@ -2,125 +2,67 @@ from prompt_generator import PromptGenerator
 from aiven_database import get_aiven_db
 from langchain.chains import create_sql_query_chain
 from google_llm import GoogleLLM
-from langchain.schema.runnable import RunnableBranch
+from langchain.schema.runnable import RunnableBranch, RunnablePassthrough
 from langchain_community.agent_toolkits.sql.base import create_sql_agent
 from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
-from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
-from operator import itemgetter
-
+from langchain_core.runnables import RunnableLambda
+from typing import Literal, Dict
 from langchain_core.output_parsers import StrOutputParser
-
+from langchain.globals import set_verbose
 
 # self.__gemini_2_0_f_lite = GoogleLLM('gemini-2.0-flash-lite', 0.0).get_llm()
 # self.__gemini_2_5_pro_exp = GoogleLLM('gemini-2.5-pro-exp-03-25', 0.0).get_llm()
 # self.__gemini_2_0_f = GoogleLLM('gemini-2.0-flash', 0.0).get_llm()
 
 class ChatBot:
+
+    def route_query(self, info: Dict) -> Literal["retrieval", "prediction", "other"]:
+        question = info["question"]
+        classification = self.__classification_chain.invoke({"question": question})
+        return classification
+
+    def handle_retrieval(self, info: Dict):
+        question = info["question"]
+        return self.__sql_agent.invoke({"input": question})
+
+    def handle_prediction(self, info: Dict):
+        question = info["question"]
+        # First get data from SQL
+        sql_result = self.__sql_agent.invoke({"input": question})
+        # Then analyze it
+        return self.__prediction_chain.invoke({"question": question, "sql_result": sql_result})
+
+    def handle_other(self, info: Dict):
+        return "I don't know"
+
     def __init__(self):
+        set_verbose(True)
         self.__prompt_generator = PromptGenerator()
         self.__gemini = GoogleLLM('gemini-2.0-flash', 0.0).get_llm()
         self.__db = get_aiven_db()
         self.__sql_toolkit = SQLDatabaseToolkit(db=self.__db, llm=self.__gemini)
-        self.__sql_agent = create_sql_agent(llm=self.__gemini, toolkit=self.__sql_toolkit, verbose=True)
+        self.__sql_agent = create_sql_agent(llm=self.__gemini, toolkit=self.__sql_toolkit)
+        classification_prompt = self.__prompt_generator.get_classification_prompt()
+        self.__classification_chain = classification_prompt | self.__gemini | StrOutputParser()
+        prediction_prompt = self.__prompt_generator.get_prediction_prompt()
+        self.__prediction_chain = prediction_prompt | self.__gemini | StrOutputParser()
         self.__run()
-        # result = self.runnable_pipeline(csv_path, query_prompt)
-
-    def runnable_pipeline(self, query_prompt, sql_chain):
-        branch = RunnableBranch(
-            (lambda x: "total sales" in x["query"], sql_chain),
-            (lambda x: "average price" in x["query"], sql_chain),
-            sql_chain  # Default chain
-        )
-
-        return branch.invoke({"query": query_prompt})
 
     def __invoke_agent(self, query: str):
         return self.__sql_agent.invoke({'input': query})['output']
 
     def __run(self):
-        classification_prompt = self.__prompt_generator.get_classification_prompt()
-        classification_chain = classification_prompt | self.__gemini | StrOutputParser()
+        full_chain = (RunnablePassthrough.assign(classification=lambda x: self.route_query(x)) |
+                      RunnableLambda(
+                          lambda x:
+                          (self.handle_retrieval(x) if x["classification"] == "retrieval"
+                          else self.handle_prediction(x) if x["classification"] == "prediction"
+                          else self.handle_other(x))))
 
-        prediction_prompt = self.__prompt_generator.get_prediction_prompt()
-        prediction_chain = prediction_prompt | self.__gemini | StrOutputParser()
-
-        result = classification_chain.invoke({'question': 'what is your name?'})
-
-        branch = RunnableBranch(
-            (lambda x: x['topic'].lower() in ["prediction", "retrieval"], self.__invoke_agent),
-            (lambda x: "other" in x['topic'].lower(), "I don't know."),
-        )
-
-        retrieval_prompt = PromptTemplate.from_template(
-            """
-                
-                Question: {question}
-                Answer:
-            """
-        )
-
-        chemistry_chain = (
-                retrieval_prompt
-                | self.__gemini
-                | StrOutputParser()
-        )
-        #
-        # prompt = PromptTemplate.from_template(
-        #     """Given the user question below, classify it as either being about `Math`, `Chemistry`, or `Other`.
-        #
-        #         Do not respond with more than one word.
-        #
-        #         <question>
-        #         {question}
-        #         </question>
-        #
-        #         Classification:"""
-        # )
-        # chain = (
-        #         {
-        #             "input": itemgetter("input")
-        #         }
-        #         | prompt
-        #         | self.__gemini
-        #         | StrOutputParser()
-        # )
-        # chain.invoke({"input": "What is t"})
-        # while True:
-        #     query = input("Ask about the data > ")
-        #     print(self.__invoke_agent(query))
-
+        question = "What will be the sales for next year in Ecuador for Minty?"
+        result = full_chain.invoke({"question": question})
+        print(result)
 
 if __name__ == "__main__":
     ChatBot()
 
-# query_chain = create_sql_query_chain(gemini_2_0_f, db)
-# question = "What is the day with most quantity of sales?"
-# sql_query = query_chain.invoke({"question": question})
-# print(sql_query)
-
-
-# sales_str = sales_df.to_string()
-#
-# template = """
-#     If you don't know the answer or the answer cannot be inferred say "I do not know". Else, answer and provide no reasoning at all.
-#     You are an expert data analyst. Base your answers only in the following csv data.
-#     {csv}
-#     {question}
-# """
-#
-# master_template = '''
-#     You are an expert data analyst. Only answer using data provided by this context.
-#     If you don't know the answer, reply "I don't know"
-# '''
-# prompt = ChatPromptTemplate.from_template(template)
-#
-# gemini_2_0_f_chain = (
-#     prompt
-#     | gemini_2_0_f
-#     | StrOutputParser()
-# )
-# # question = 'What is 2 + 2 ?'
-# # question = 'What is the day that has the most quantity of sales?'
-# while True:
-#     question = input('Type your question: ')
-#     print(gemini_2_0_f_chain.invoke({'question': question, 'csv': sales_str}))
--- chatbot/router.py
+++ chatbot/router.py
@@ -2,7 +2,7 @@ from typing import Literal, Dict
 from langchain.agents.agent import AgentExecutor
 
 class ChainRouter:
-    def __init__(self, classifier_chain, sql_agent: AgentExecutor, prediction_chain):
+    def __init__(self, classifier_chain, prediction_chain, sql_agent: AgentExecutor):
         self.__classifier_chain = classifier_chain
         self.__sql_agent = sql_agent
         self.__prediction_chain = prediction_chain
@@ -21,7 +21,7 @@ class ChainRouter:
         # First get data from SQL
         sql_result = self.__sql_agent.invoke({"input": question})
         # Then analyze it
-        return prediction_chain.invoke({"question": question, "sql_result": sql_result})
+        return self.__prediction_chain.invoke({"question": question, "sql_result": sql_result})
 
-    def handle_other(info: Dict):
+    def handle_other(self, info: Dict):
         return "I don't know"
